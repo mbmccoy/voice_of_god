b156dc92201b27c26c14c9a27a784eaea1e31ee9
Michael McCoy <mccoy@cofacet.com>
2015-02-25 17:16:10 -0800

    Updated procfile.

--- server.py
+++ server.py
@@ -39,4 +39,4 @@ def flask_decode():
         return "Heresy! " + str(h)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', '5000')))
